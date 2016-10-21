23d407ee18138cde403a0865ce9fd4b7c55a7384
Bernadette <bernadette@englemaninvest.com>
2016-10-21 15:34:00 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,24 +1,7 @@
-
 // Copyright Year
 var d = new Date(),
     n = d.getFullYear();
 document.getElementById("year").innerHTML = n;
-
-$(document).ready(function() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-
-        //Call weather today
-        loadWeather(lat+','+lon);
-
-        //Call Forecast
-        loadForecast(lat+','+lon);
-
-        //Call Location Map
-        getLocation();
-    });
-});
 
 //Get weather using lat/lng coordinates
 function loadWeather(location, woeid) {
@@ -29,15 +12,15 @@ function loadWeather(location, woeid) {
         success: function(weather) {
 
             /*
-            if (weather.alt.temp <= 5) {
-                document.getElementById("changeBG").className = "below5";
-            } else if (weather.alt.temp <= 15) {
-                document.getElementById("changeBG").className = "below15";
-            } else if (weather.alt.temp <= 25) {
-                document.getElementById("changeBG").className = "below25";
-            } else {
-                document.getElementById("changeBG").className = "hot";
-            }
+             if (weather.alt.temp <= 5) {
+             document.getElementById("changeBG").className = "below5";
+             } else if (weather.alt.temp <= 15) {
+             document.getElementById("changeBG").className = "below15";
+             } else if (weather.alt.temp <= 25) {
+             document.getElementById("changeBG").className = "below25";
+             } else {
+             document.getElementById("changeBG").className = "hot";
+             }
              */
 
             document.getElementById("temperature").innerHTML = weather.temp+'&deg;'+weather.units.temp+' / '+weather.alt.temp+'&deg;C';
@@ -45,11 +28,11 @@ function loadWeather(location, woeid) {
             document.getElementById("weatherToday").innerHTML = weather.currently;
 
             /*
-            html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+' / '+weather.alt.temp+'&deg;C</h2>';
-            html += '<h3>'+weather.city+', '+weather.region+'</h3>';
-            html += '<h4 class="currently">'+weather.currently+'</h4>';
-            $("#weather").html(html);
-            */
+             html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+' / '+weather.alt.temp+'&deg;C</h2>';
+             html += '<h3>'+weather.city+', '+weather.region+'</h3>';
+             html += '<h4 class="currently">'+weather.currently+'</h4>';
+             $("#weather").html(html);
+             */
         },
 
         error: function(error) {
@@ -66,7 +49,7 @@ function loadForecast(location, woeid) {
         unit: 'f',
         success: function (weather) {
             html = '<h2 id="sameFont">5-Day Forecast</h2>';
-            html += '<p>Day &nbsp; Low / High</p>'
+            html += '<p>Day &nbsp; Low / High</p>';
 
             for (var i = 0; i < 5; i++) {
                 html += '<p>' + weather.forecast[i].day + ': ' + weather.forecast[i].low + '&deg;' + weather.units.temp + ' / ' + weather.forecast[i].high + '&deg;' + weather.units.temp + '</p>';
@@ -113,5 +96,21 @@ function showPosition(position) {
     var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
     var marker = new google.maps.Marker({position:latlon,map:map,title:"Your location!"});
 }
+
+$(document).ready(function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+
+        //Call weather today
+        loadWeather(lat+','+lon);
+
+        //Call Forecast
+        loadForecast(lat+','+lon);
+
+        //Call Location Map
+        getLocation();
+    });
+});
 
 //alert(windowWidth);
